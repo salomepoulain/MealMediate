d2a88212a3bd09fff765c745b0d0fc1c1ccff8dc
Salomé Poulain <salome.poulain@student.uva.nl>
2023-11-22 09:43:58 +0100

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Salome Poulain on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
