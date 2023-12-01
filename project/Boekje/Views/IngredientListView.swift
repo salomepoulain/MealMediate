938244215295ec4559e0d2ccc4a2606fa26ad65d
Salomé Poulain <salome.poulain@student.uva.nl>
2023-12-01 16:04:35 +0100

    Kleine aanpassingen in creëer recept form, aspectratio van afbeeldingen gefixt. Nieuw model aangemaakt voor verschillende ingredienten en relatie aangemaakt. Toegevoegd in form. Enige wat nog mist: geselecteerde ingredienten opslaan in model. Hierdoor helaas versie 1 deadline niet helemaal af, verder gaan in dit weekend.

---
+++ project/Boekje/Views/IngredientListView.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientListView.swift
+//  project
+//
+//  Created by Salome Poulain on 01/12/2023.
+//
+
+import SwiftUI
+
+struct IngredientListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IngredientListView()
+}
